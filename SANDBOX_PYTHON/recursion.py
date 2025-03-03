767555843cdf35020d1ddf4a36763863aa0285bd
Tej Singh <tejpratap.singh6@gmail.com>
2025-03-03 08:26:13 -0600

    rtwqtqe4

--- SANDBOX_PYTHON/recursion.py
+++ SANDBOX_PYTHON/recursion.py
@@ -6,4 +6,15 @@ def get_factorial(n):
         return n*get_factorial(n-1)
     
 
-print(get_factorial(5))
+#print(get_factorial(5))
+
+n=5
+series=[]
+for i in range(n):
+    if i==0:
+        series.append(0)
+    elif i==1:
+        series.append(1)
+    else:
+        series.append(series[i-2]+series[i-1])
+print(series)
