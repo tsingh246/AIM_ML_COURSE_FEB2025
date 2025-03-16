11ab5af98193fedb821f16e5ef422a4477744f22
Tej Singh <tejpratap.singh6@gmail.com>
2025-03-15 22:26:36 -0500

    hggh

--- SANDBOX_PYTHON/combinations.py
+++ SANDBOX_PYTHON/combinations.py
@@ -5,35 +5,35 @@ Output: ["AB","AC","BC"]
 # for k= 2
 '''
 
-list = ["A","B","C","D"]
-out_list=[]
+list = ["A","B","C","D","E"]
+out_list_2=[]
 for i in range(len(list)):
     #print(list[i])
     for j in range(i+1,len(list)):
-        out_list.append(list[i]+list[j])
+        out_list_2.append(list[i]+list[j])
         #print(list[i]+list[j])
-#print(out_list)
+print(out_list_2)
 # for k= 3
-list = ["A","B","C","D"]
-out_list=[]
+
+out_list_3=[]
 for i in range(len(list)):
     #print(list[i])
     for j in range(i+1,len(list)):
         for k in range(j+1,len(list)):
-            out_list.append(list[i]+list[j]+list[k])
+            out_list_3.append(list[i]+list[j]+list[k])
         #print(list[i]+list[j])
-#print(out_list)
+print(out_list_3)
 
 # for k= 4
-list = ["A","B","C","D"]
-out_list=[]
+
+out_list_4=[]
 for i in range(len(list)):
     #print(list[i])
     for j in range(i+1,len(list)):
         for k in range(j+1,len(list)):
             for l in range(k+1,len(list)):
-                out_list.append(list[i]+list[j]+list[k]+list[l])
+                out_list_4.append(list[i]+list[j]+list[k]+list[l])
         #print(list[i]+list[j])
-print(out_list)
+print(out_list_4)
     
 
