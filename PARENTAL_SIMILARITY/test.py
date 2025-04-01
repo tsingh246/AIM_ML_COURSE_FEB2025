2996af82fe2e81a7fc59a558ac4a001f8b4eda81
Tej Singh <tejpratap.singh6@gmail.com>
2025-04-01 10:15:49 -0500

    ewe

--- PARENTAL_SIMILARITY/test.py
+++ PARENTAL_SIMILARITY/test.py
@@ -1,1 +1,63 @@
-print("new project")
+import os
+from IPython.display import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from matplotlib import rcParams
+import face_recognition
+
+
+def load_images(known_images_dir):
+    known_encodings = []
+    known_images = []
+
+    for file in os.listdir(known_images_dir):
+        #fsdecode function decode the file into 
+       # print(file)
+        filename = os.fsdecode(file)
+        #print(filename)
+       # print(os.path.join(known_images_dir, filename))
+        image = face_recognition.load_image_file(os.path.join(known_images_dir, filename))
+        #print("image  ",image)
+
+        enc = face_recognition.face_encodings(image)
+       # print("enc   ",enc)
+        if len(enc) > 0:
+            known_encodings.append(enc[0])
+            known_images.append(filename)
+
+    return (known_encodings, known_images)
+
+
+print(load_images("C:\Learning\images\paternal"))
+
+
+'''
+
+def calculate_face_distance(known_encodings, unknown_img_path, cutoff=0.5, num_results=4):
+    image_to_test = face_recognition.load_image_file(unknown_img_path)
+    image_to_test_encoding = face_recognition.face_encodings(image_to_test)[0]
+
+    face_distances = face_recognition.face_distance(known_encodings, image_to_test_encoding)
+    return (unknown_img_path, known_images[face_distances.argmin()])
+
+known_encodings, known_images = load_images("/cxldata/projects/lookalikeceleb/images")
+original_image = "../lookalikeceleb/myimage.jpg"
+Image(filename=original_image)
+
+matching_image = calculate_face_distance(known_encodings, original_image)
+#%matplotlib inline
+
+# read images
+img_1 = mpimg.imread(original_image)
+img_2 = mpimg.imread('/cxldata/projects/lookalikeceleb/images/' + matching_image[1])
+
+# display images
+fig, ax = plt.subplots(1,2)
+ax[0].imshow(img_1);
+ax[1].imshow(img_2);
+
+print('Hey, you look like ' + os.path.splitext(matching_image[0]) + '!')
+
+
+'''
